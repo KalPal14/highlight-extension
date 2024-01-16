6c07f6e3d0dfc0f394b03079606265d7883f8810
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2024-01-16 11:46:45 +0200

    feat: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,19 @@ import App from '@/app';
 import { LoggerService } from '@/common/services/logger.service';
 import { UsersController } from '@/users/users.controller';
 import { ConfigService } from '@/common/services/config.service';
+import { ExceptionFilter } from '@/errors/exception.filter';
 import TYPES from '@/types.inversify';
 import { ILogger } from '@/common/services/logger.service.interface';
 import { IUsersController } from '@/users/users.controller.interface';
 import { IConfigService } from '@/common/services/config.service.interface';
+import { IExceptionFilter } from '@/errors/exception.filter.interface';
 
 const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<App>(TYPES.App).to(App);
 	bind<ILogger>(TYPES.LoggerService).to(LoggerService);
 	bind<IUsersController>(TYPES.UsersController).to(UsersController);
 	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
 });
 
 function bootstrap(): void {
