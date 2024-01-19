cd5339b4b84e74494cb537e8bc4acf3c58ee771b
KalPal <reznichenkomykola1@gmail.com>
2024-01-19 11:31:54 +0200

    setup/prisma seed and modify package.json scripts for working with prisma

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,14 @@ import { BaseController } from '@/common/base.controller';
 import TYPES from '@/types.inversify';
 import { IUsersController } from './users.controller.interface';
 import { IConfigService } from '@/services/config.service.interface';
+import { IPrismaService } from '@/services/prisma.service.interface';
 
 @injectable()
 export class UsersController extends BaseController implements IUsersController {
-	constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {
+	constructor(
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.PrismaService) private prismaService: IPrismaService,
+	) {
 		super();
 		this.bindRoutes([
 			{
@@ -21,10 +25,10 @@ export class UsersController extends BaseController implements IUsersController 
 	}
 
 	async test({ body }: Request, res: Response, next: NextFunction): Promise<void> {
+		const users = await this.prismaService.client.user.findMany();
+
 		this.ok(res, {
-			msg: 'TEST!!!',
-			body,
-			conf: this.configService.get('TEST'),
+			users,
 		});
 	}
 }
