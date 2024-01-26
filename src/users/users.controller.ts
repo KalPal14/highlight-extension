7ba717f8afb64c28601776df620756175357cb41
KalPal <reznichenkomykola1@gmail.com>
2024-01-26 12:08:12 +0200

    feature/add registration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { Request, Response, NextFunction } from 'express';
 import { inject, injectable } from 'inversify';
+import { sign } from 'jsonwebtoken';
 
 import { BaseController } from '@/common/base.controller';
 import TYPES from '@/types.inversify';
@@ -10,10 +11,16 @@ import { USERS_PATH } from '@/common/constants/routes/users';
 import { UsersRegisterDto } from './dto/users-register.dto';
 import { ValidateMiddleware } from '@/common/middlewares/validate.middleware';
 import { HTTPError } from '@/errors/http-error.class';
+import { IConfigService } from '@/common/services/config.service.interface';
+import { User } from './user.entity';
+import { UserModel } from '@prisma/client';
 
 @injectable()
 export class UsersController extends BaseController implements IUsersController {
-	constructor(@inject(TYPES.UsersService) private usersService: IUsersService) {
+	constructor(
+		@inject(TYPES.UsersService) private usersService: IUsersService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+	) {
 		super();
 		this.bindRoutes([
 			{
@@ -37,6 +44,24 @@ export class UsersController extends BaseController implements IUsersController 
 		]);
 	}
 
+	private async signJwt({ id, email, username }: UserModel): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const jwtKey = this.configService.get('JWT_KEY');
+			if (jwtKey instanceof Error) {
+				reject(jwtKey.message);
+				return;
+			}
+
+			sign({ id, email, username }, jwtKey, { algorithm: 'HS256' }, (err, token) => {
+				if (err) {
+					reject(err.message);
+				} else {
+					resolve(token as string);
+				}
+			});
+		});
+	}
+
 	async login({ body }: Request, res: Response, next: NextFunction): Promise<void> {
 		this.ok(res, 'login');
 	}
@@ -48,12 +73,22 @@ export class UsersController extends BaseController implements IUsersController 
 	): Promise<void> {
 		const result = await this.usersService.createUser(body);
 
-		if (result instanceof HTTPError) {
+		if (result instanceof Error) {
 			next(result);
 			return;
 		}
 
-		this.created(res, result);
+		this.signJwt(result)
+			.then((jwt) =>
+				this.created(res, {
+					jwt,
+				}),
+			)
+			.catch((err) =>
+				this.send(res, 500, {
+					err,
+				}),
+			);
 	}
 
 	async logout({ body }: Request, res: Response, next: NextFunction): Promise<void> {
