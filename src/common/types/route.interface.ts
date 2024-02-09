c8ba16a34b9c22027f13ed06f60dcbe1dfed79f9
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2024-02-09 10:27:27 +0200

    refactor: enhance controller type safety and clarity

--- src/common/types/route.interface.ts
+++ src/common/types/route.interface.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
 
 import { IMiddleware } from '../middlewares/middleware.interface';
 import { TController } from './controller.type';
