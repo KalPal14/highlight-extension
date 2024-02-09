0d7c66c4901fbf770efa2225746d04dceae0b29c
KalPal <reznichenkomykola1@gmail.com>
2024-02-09 10:27:27 +0200

    refactored/improved controller typing

--- src/common/types/controller.type.ts
+++ src/common/types/controller.type.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
-export type TController<Params = any, ResBody = any, ReqBody = any, ReqQuery = any> = (
+import { IObject } from './object.interface';
+
+export type TController<
+	Params extends IObject<string> = any,
+	ResBody extends IObject<any> = any,
+	ReqBody extends IObject<any> = any,
+	ReqQuery extends IObject<string> = any,
+> = (
 	req: Request<Params, ResBody, ReqBody, ReqQuery>,
 	res: Response<ResBody>,
 	next: NextFunction,
