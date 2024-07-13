a57fda0c6c70e97a1acbbf228508adb99bb1608a
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2024-07-13 16:36:28 +0300

    feat: add wrapper for controllers to handle unexpected errors

--- src/controllers/base-controller/base.controller.ts
+++ src/controllers/base-controller/base.controller.ts
@@ -2,7 +2,11 @@ import 'reflect-metadata';
 import { Response, Router } from 'express';
 import { injectable } from 'inversify';
 
+import { TController } from '../common/types/controller.type';
+
 import { IRouteController } from './types/route.interface';
+
+import { HTTPError } from '@/exceptions/http-error.class';
 
 @injectable()
 export abstract class BaseController {
@@ -32,7 +36,13 @@ export abstract class BaseController {
 	protected bindRoutes(routes: IRouteController[]): void {
 		routes.forEach((route) => {
 			const middlewares = route.middlewares?.map((m) => m.execute.bind(m));
-			const handler = route.func.bind(this);
+			const handler: TController = async (req, res, next) => {
+				try {
+					await route.func.bind(this)(req, res, next);
+				} catch {
+					next(new HTTPError(500, 'Unexpected error'));
+				}
+			};
 			const pipeline = middlewares ? [...middlewares, handler] : handler;
 			this._router[route.method](route.path, pipeline);
 		});
