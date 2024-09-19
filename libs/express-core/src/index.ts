ff53270a80f5b6ffcaa32530ee83941e12bddd55
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2024-09-19 18:27:37 +0200

    refactor(express-core): reorganize folder structure

--- libs/express-core/src/index.ts
+++ libs/express-core/src/index.ts
@@ -1,8 +1,5 @@
-// common
-export * from './common/base-controller/base.controller';
-
 // constants
-export * from './constants/types';
+export * from './common/constants/types';
 
 // exceptions
 export * from './exceptions/exception-filter/exception.filter.interface';
@@ -10,7 +7,7 @@ export * from './exceptions/exception-filter/exception.filter';
 
 // middlewares
 export * from './middlewares/common/types/middleware.interface';
-export * from './middlewares/jwt-auth.middleware';
+export * from './middlewares/jwt-auth/jwt-auth.middleware';
 export * from './middlewares/validate/validate.middleware';
 export * from './middlewares/role-guard/role.guard';
 
@@ -26,4 +23,6 @@ export * from './services/prisma-service/base-prisma.service.interface';
 export * from './common/types/controller.type';
 
 // utils
-export * from './utils/bindings/express-core.bindings';
+export * from './common/base-controller/base.controller';
+export * from './common/base-controller/types/route.interface';
+export * from './common/bindings/express-core.bindings';
