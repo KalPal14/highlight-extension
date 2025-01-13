3c0f4ce2065b24cc4f57db462b73b8a86dd84d03
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-01-13 14:47:04 +0200

    feat(freq-words): basic setup with unit tests, ConfigModule, and TypeOrmModule

--- apps/freq-words/src/app.module.ts
+++ apps/freq-words/src/app.module.ts
@@ -1,10 +1,33 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: join(
+				__dirname,
+				process.env.NODE_ENV !== 'test' ? '' : '../../',
+				'..',
+				`.env.${process.env.NODE_ENV || 'dev'}`
+			),
+		}),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: process.env.FREQ_WORDS_HOST,
+			port: +process.env.FREQ_WORDS_DB_PORT,
+			username: process.env.FREQ_WORDS_DB_USERNAME,
+			password: process.env.FREQ_WORDS_DB_PASSWORD,
+			database: process.env.FREQ_WORDS_DB_NAME,
+			autoLoadEntities: true,
+			synchronize: process.env.NODE_ENV !== 'prod',
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
