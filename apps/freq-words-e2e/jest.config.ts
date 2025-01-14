816cebc26dac5079f39c93e2014345332260cd24
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-01-14 15:21:19 +0200

    feat(freq-words): add freq-words-e2e app

---
+++ apps/freq-words-e2e/jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+	verbose: true,
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testRegex: 'e2e-spec.ts',
+	coverageDirectory: 'coverage',
+	moduleNameMapper: {
+		// apps,
+		'^~/freq-words/(.*)$': '<rootDir>/../freq-words/src/$1',
+		'^~/iam/prisma/(.*)$': '<rootDir>/../iam/prisma/$1',
+		'^~/iam/(.*)$': '<rootDir>/../iam/src/$1',
+		// libs
+		'^~libs/common': '<rootDir>/../../libs/common/src',
+		'^~libs/dto/(.*)$': '<rootDir>/../../libs/dto/src/$1',
+		'^~libs/express-core': '<rootDir>/../../libs/express-core/src',
+		'^~libs/routes/(.*)$': '<rootDir>/../../libs/routes/src/$1',
+	},
+};
+
+export default config;
