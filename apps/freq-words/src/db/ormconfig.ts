cb15a1284769182a7f53236a31e434cffa3f87d9
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-01-24 11:01:34 +0200

    feat(freq-words): add LanguageSeeder for initial data population

---
+++ apps/freq-words/src/db/ormconfig.ts
@@ -0,0 +1,26 @@
+import { join } from 'path';
+
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+import { SeederOptions } from 'typeorm-extension';
+
+import LanguageSeeder from './seeds/languages/language.seeder';
+
+config({
+	path: join(__dirname, `../../../../../.env.${process.env.NODE_ENV || 'dev'}`),
+});
+
+const options: DataSourceOptions & SeederOptions = {
+	type: 'postgres',
+	host: process.env.FREQ_WORDS_HOST,
+	port: +process.env.FREQ_WORDS_DB_PORT,
+	username: process.env.FREQ_WORDS_DB_USERNAME,
+	password: process.env.FREQ_WORDS_DB_PASSWORD,
+	database: process.env.FREQ_WORDS_DB_NAME,
+	synchronize: process.env.NODE_ENV !== 'prod',
+	entities: [join(__dirname, '..', 'resources', '**', 'entities', '*.entity.js')],
+	migrations: [join(__dirname, 'migrations', '*.js')],
+	seeds: [LanguageSeeder],
+};
+
+export const dataSource = new DataSource(options);
---
+++ apps/freq-words/src/db/seeds/languages/language.seeder.ts
@@ -0,0 +1,47 @@
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { DataSource } from 'typeorm';
+
+import { Language } from '~/freq-words/resources/languages/entities/language.entity';
+import {
+	ENGLISH_LANGUAGE_ENTITY,
+	URKAINIAN_LANGUAGE_ENTITY,
+} from '~/freq-words/resources/languages/stubs/languages';
+
+export default class LanguageSeeder implements Seeder {
+	public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<void> {
+		await dataSource.query('TRUNCATE "language" RESTART IDENTITY CASCADE;');
+
+		const repository = dataSource.getRepository(Language);
+		await repository.insert([
+			ENGLISH_LANGUAGE_ENTITY,
+			URKAINIAN_LANGUAGE_ENTITY,
+			{ name: 'Russian' },
+			{ name: 'German' },
+			{ name: 'Portuguese' },
+			{ name: 'Arabic' },
+			{ name: 'Greek' },
+			{ name: 'Romanian' },
+			{ name: 'Bulgarian' },
+			{ name: 'Hungarian' },
+			{ name: 'Chinese' },
+			{ name: 'Indonesian' },
+			{ name: 'Slovak' },
+			{ name: 'Czech' },
+			{ name: 'Italian' },
+			{ name: 'Slovenian' },
+			{ name: 'Danish' },
+			{ name: 'Japanese' },
+			{ name: 'Spanish' },
+			{ name: 'Dutch' },
+			{ name: 'Latvian' },
+			{ name: 'Estonian' },
+			{ name: 'Lithuanian' },
+			{ name: 'Finnish' },
+			{ name: 'Swedish' },
+			{ name: 'Turkish' },
+			{ name: 'Norwegian' },
+			{ name: 'French' },
+			{ name: 'Polish' },
+		]);
+	}
+}
