eb7cdf34e4837516af689402b9a179d79497c9b8
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-03-11 13:20:03 +0200

    feat(h-ext-fe): add extension-control page

--- apps/highlight-extension-fe/src/app/ui/entries/popup/index.tsx
+++ apps/highlight-extension-fe/src/app/ui/entries/popup/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { ExtensionControlPage } from '~/highlight-extension-fe/pages/extension-control';
 
 function init(): void {
 	const rootContainer = document.createElement('div');
 	document.body.appendChild(rootContainer);
 	const root = createRoot(rootContainer);
-	root.render(<h1>popup</h1>);
+	root.render(
+		<ChakraProvider>
+			<ExtensionControlPage />
+		</ChakraProvider>
+	);
 }
 
 init();
