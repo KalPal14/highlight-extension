b4138775cf5fd99ffe3a95d9ded9c838c6e66b54
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-03-11 12:45:44 +0200

    feat(react-core): create react-core library with react svg assets

---
+++ libs/react-core/src/index.ts
@@ -0,0 +1,9 @@
+// /assets/imgs/svg
+export * from './assets/imgs/svg/angle-down';
+export * from './assets/imgs/svg/angle-up';
+export * from './assets/imgs/svg/cog';
+export * from './assets/imgs/svg/exclamation-octagon';
+export * from './assets/imgs/svg/eye';
+export * from './assets/imgs/svg/eye-slash';
+export * from './assets/imgs/svg/pencil-square';
+export * from './assets/imgs/svg/trash';
