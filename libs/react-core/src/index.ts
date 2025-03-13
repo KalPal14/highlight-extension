72068baaa252d19d656eccf5c7054e7663ab6f7c
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-03-13 22:23:04 +0200

    feat(react-core): add GripVerticalSVG component

--- libs/react-core/src/index.ts
+++ libs/react-core/src/index.ts
@@ -5,16 +5,20 @@ export * from './assets/imgs/svg/cog';
 export * from './assets/imgs/svg/exclamation-octagon';
 export * from './assets/imgs/svg/eye';
 export * from './assets/imgs/svg/eye-slash';
+export * from './assets/imgs/svg/grip-vertical';
 export * from './assets/imgs/svg/pencil-square';
 export * from './assets/imgs/svg/trash';
 
 // hooks
 export * from './hooks/cross-browser-state/cross-browser-state-builder';
 export * from './hooks/exception-handler/exception-handler-hook';
+export * from './hooks/tabs/tab';
+export * from './hooks/tabs/tabs-hook';
 
 // ui
 export * from './ui/alerts/high-alert';
 export * from './ui/alerts/outside-click-alert';
 export * from './ui/fields/text-field';
+export * from './ui/forms/accordion-form';
 export * from './ui/toasts/toasts-options';
 export * from './ui/wrappers/viewport-centered/viewport-centered';
