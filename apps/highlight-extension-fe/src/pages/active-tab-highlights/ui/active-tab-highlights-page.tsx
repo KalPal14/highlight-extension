eaa1dab279459766cd7d63490dcaa05d589b988d
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-03-14 17:27:14 +0200

    feat(h-ext-fe): add active-tab-highlights page

---
+++ apps/highlight-extension-fe/src/pages/active-tab-highlights/ui/active-tab-highlights-page.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Alert, AlertIcon, Tooltip } from '@chakra-ui/react';
+
+import { CogSVG } from '~libs/react-core';
+import { openTab } from '~libs/client-core';
+
+import { FULL_OPTIONS_ROUTES } from '~/highlight-extension-fe/shared/ui';
+import { LinkToLogin } from '~/highlight-extension-fe/widgets/link-to-login';
+import { useCrossBrowserState } from '~/highlight-extension-fe/shared/model';
+
+import './styles.scss';
+
+export function ActiveTabHighlightsPage(): JSX.Element {
+	const [jwt] = useCrossBrowserState('jwt');
+
+	return (
+		<div className="activeTabHighlightsPage">
+			<header className="activeTabHighlightsPage_header">
+				<Tooltip
+					label="Settings"
+					fontSize="md"
+					placement="left"
+				>
+					<div>
+						<CogSVG
+							onClick={() => openTab(FULL_OPTIONS_ROUTES.pages)}
+							height={28}
+							width={28}
+							cursor="pointer"
+						/>
+					</div>
+				</Tooltip>
+			</header>
+			<main className="highlightsList">
+				{!jwt && (
+					<>
+						<Alert
+							status="info"
+							mt={4}
+							mb={4}
+						>
+							<AlertIcon />
+							All the notes you have on the active page will appear here. Please log in to continue
+						</Alert>
+						<LinkToLogin />
+					</>
+				)}
+				{Boolean(jwt) && <h1>HighlightsListTabs</h1>}
+			</main>
+		</div>
+	);
+}
