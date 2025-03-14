ac21c51c28db0bb701b6c9842d80a3e968fbb75b
KalPal14 <56432055+KalPal14@users.noreply.github.com>
2025-03-14 12:06:20 +0200

    feat(h-ext-fe): add useHighlights for highlight management

--- apps/highlight-extension-fe/src/shared/model/hooks/cross-browser-state/types/cross-browser-state-values.interface.ts
+++ apps/highlight-extension-fe/src/shared/model/hooks/cross-browser-state/types/cross-browser-state-values.interface.ts
@@ -1,8 +1,16 @@
-import { IBaseWorkspaceRo } from '~libs/ro/highlight-extension';
+import {
+	IBaseWorkspaceRo,
+	ICreateHighlightRo,
+	IDeleteHighlightRo,
+	IUpdateHighlightRo,
+} from '~libs/ro/highlight-extension';
 import { IBaseUserRo } from '~libs/ro/iam';
 
 export interface ICrossBrowserStateDescriptor {
 	jwt: string | null;
 	currentUser: IBaseUserRo | null;
 	currentWorkspace: IBaseWorkspaceRo | null;
+	createdHighlight: { highlight: ICreateHighlightRo; pageUrl: string } | null;
+	deletedHighlight: { highlight: IDeleteHighlightRo; pageUrl: string } | null;
+	updatedHighlight: { highlight: IUpdateHighlightRo; pageUrl: string } | null;
 }
